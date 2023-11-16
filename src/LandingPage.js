63d39168bea5cd5b9d72938c1cfdc197d694f458
hassaan Arien <hassanarien@gmail.com>
2023-11-16 14:10:08 -0600

    first

---
+++ src/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/LandingPage.css';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>This Is My Landing Page</h1>
+    </div>
+  );
+};
+
+export default LandingPage;
---
+++ src/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles/NavBar.css';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Products</li>
+        <li>Profile</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
